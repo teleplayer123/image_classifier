d6eaaa9ff273b1164a900638cddf2acebd4da4c3
cole sashkin <csashceh1024@gmail.com>
2024-08-04 14:46:29 -0700

    update

--- train_model.py
+++ train_model.py
@@ -1,3 +1,10 @@
 import tensorflow as tf
 
 
+def create_model():
+    model = tf.keras.Sequential([
+    tf.keras.layers.Dense(10, activation=tf.nn.relu, input_shape=(20,)),
+    tf.keras.layers.Dense(10, activation=tf.nn.relu),
+    tf.keras.layers.Dense(3)
+    ])
+    
