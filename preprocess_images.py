faa2ee44f019daea3591f6488ee99c3256eef1ff
cole sashkin <csashceh1024@gmail.com>
2024-08-04 14:33:53 -0700

    update

--- preprocess_images.py
+++ preprocess_images.py
@@ -110,6 +110,7 @@ def train_predict_digits(X, y, digit=1):
     X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
     y_train_digit = (y_train == digit)
     y_test_digit = (y_test == digit)
+    #stochastic gradient descent linear model
     sgd_clf = SGDClassifier(random_state=42)
     sgd_clf.fit(X_train, y_train_digit)
     sgd_clf.predict(X_test[0])
