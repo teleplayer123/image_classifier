55d9f496cb00dd6536ab4687ac4b60bfa3fe82a8
Cole Sashkin <csashceh1024@gmail.com>
2024-08-14 15:20:22 -0700

    update

--- test.py
+++ test.py
@@ -2,9 +2,9 @@ from pycoral.utils.edgetpu import make_interpreter
 from image_classification import classify_image
 import os
 
-labels = os.path.join(os.getcwd(), "models", "labels.txt")
+labels = os.path.join(os.getcwd(), "models_int", "labels.txt")
 input_img = os.path.join(os.getcwd(), "integer_image_dataset", "image89_0.png")
-model_file = os.path.join(os.getcwd(), "models", "model.tflite")
+model_file = os.path.join(os.getcwd(), "models_int", "model.tflite")
 
 interpreter = make_interpreter(model_file)
 
