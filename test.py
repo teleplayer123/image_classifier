437b7343a61c5af5907982b3bbd419bb01b8588e
cole sashkin <csashceh1024@gmail.com>
2024-08-18 00:32:20 -0700

    update

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import os
 
 labels = os.path.join(os.getcwd(), "models", "labels.txt")
 # input_img = os.path.join(os.getcwd(), "digit_images_dataset", "9_nine", "image9_0.png")
-input_img = os.path.join(os.getcwd(), "integer_image_dataset", "31", "image31_0.png")
+input_img = os.path.join(os.getcwd(), "integer_image_dataset", "32", "image32_0.png")
 model_file = os.path.join(os.getcwd(), "models", "model.tflite")
 
 interpreter = make_interpreter(model_file)
