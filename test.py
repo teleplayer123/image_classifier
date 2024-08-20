08d5ff264ab3215cc1435e2c808c85aa3cc9b291
cole sashkin <csashceh1024@gmail.com>
2024-08-20 00:54:03 -0700

    update

--- test.py
+++ test.py
@@ -4,9 +4,15 @@ import os
 
 labels = os.path.join(os.getcwd(), "models", "labels.txt")
 # input_img = os.path.join(os.getcwd(), "digit_images_dataset", "9_nine", "image9_0.png")
-input_img = os.path.join(os.getcwd(), "integer_image_dataset", "32", "image32_0.png")
+# input_img = os.path.join(os.getcwd(), "integer_image_dataset", "32", "image32_0.png")
 model_file = os.path.join(os.getcwd(), "models", "model.tflite")
 
 interpreter = make_interpreter(model_file)
 
-classify_image(input_img, labels, interpreter)
+for i in range(20):
+    try:
+        input_img = os.path.join(os.getcwd(), "integer_image_dataset", "{}".format(i), "image{}_0.png".format(i))
+        classify_image(input_img, labels, interpreter)
+        print("Actual value: {}".format(i))
+    except TypeError:
+        continue
